c5e05de8c7b94d07ee71f4e491b7910d9e01b319
tachmi <vladonlesov@gmail.com>
2025-03-20 22:01:34 +0300

    sdelal menu dlya studentov 😒

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,4 +53,5 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:33.10.0"))
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-auth-ktx")
+    implementation("com.google.android.material:material:1.11.0")
 }
